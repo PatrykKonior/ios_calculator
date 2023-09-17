1b8b428f23690f08e5fe3513eca6ffa20e87455e
Patryk <patrykkonior4@gmail.com>
2023-09-17 11:38:31 +0200

    Get particular number and symbols from calculator

--- calculator.js
+++ calculator.js
@@ -1,9 +1,38 @@
+let buffer = '0';
 
+
+function buttonClick(value) {
+    if (isNaN(parseInt(value))) {
+       handleSymbol(value); 
+    } else {
+        handleNumber(value);
+    }
+
+}
+
+
+function handleNumber(number) {
+    if (buffer === '0') {
+        buffer = number
+    } else {
+        buffer += number;
+    }
+
+}
+
+
+function handleSymbol(symbol) {
+    console.log('symbol');
+
+}
 
 
 function init() {
-    document.querySelector('.calc-buttons')
+    document
+        .querySelector('.calc-buttons')
         .addEventListener("click", function(event) {
             buttonClick(event.target.innerText);
         })
 }
+
+init();
